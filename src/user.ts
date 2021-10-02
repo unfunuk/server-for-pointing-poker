f0318b782e3dd8d199770c8998f09a5e44febe6b
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-10-03 01:09:55 +0300

    feat: change arguments

--- src/user.ts
+++ src/user.ts
@@ -3,21 +3,21 @@ import mongoose from "mongoose";
 export interface UserInterface {
   id: string;
   firstName: string;
-  lastName: string;
+  lastName?: string;
   role: string;
   initials: string;
-  jobPosition: string;
-  avatarSource: string;
+  jobPosition?: string;
+  avatarSource?: string;
   sessionId: string;
 }
 const User = new mongoose.Schema({
   id: { type: String, required: true },
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: false },
   role: { type: String, required: true },
   initials: { type: String, required: true },
-  jobPosition: { type: String, required: true },
-  avatarSource: { type: String, required: true },
+  jobPosition: { type: String, required: false },
+  avatarSource: { type: String, required: false },
   sessionId: { type: String, required: true },
 });
 export default mongoose.model("user", User);
