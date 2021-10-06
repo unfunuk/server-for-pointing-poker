7bc61f2102c57a7451124a5113ce4d8f58beb05c
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-10-06 18:34:08 +0300

    feat: add headingText

--- src/session.ts
+++ src/session.ts
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 export interface SessionInterface {
   sessionId: string;
   isGameStarted: boolean;
+  headingText: string;
 }
 const Session = new mongoose.Schema({
   sessionId: { type: String, required: true },
   isGameStarted: { type: Boolean, required: true },
+  headingText: { type: String, required: true },
 });
 export default mongoose.model("session", Session);
